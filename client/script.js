0eaec2bacdf97c5f6a0b1d4b7cd125261fd2c7a3
joerdixon <joseph35dixon@gmail.com>
2022-11-08 14:35:52 -0800

    dun

--- client/script.js
+++ client/script.js
@@ -1,3 +1,4 @@
+// Import io function from Socket.io client package.
 import { io } from "socket.io-client";
 
 // The message text box
@@ -8,6 +9,21 @@ const roomInput = document.getElementById("room-input");
 const joinRoomButton = document.getElementById("room-button");
 // The whole form (the submit handler is on the send message button)
 const form = document.getElementById("form");
+
+// Create a socket pointed at where we want to host it.
+// This is the connection between the two Socket.io packages (serverside and client)???
+const socket = io("http://localhost:3000")
+// When somebody connects to the socket, print a connection message to chatspace with their socket id.
+socket.on("connect", () => {
+    displayMessage(`Connected with id: ${socket.id}`)
+})
+
+socket.on("recieve-message", message => {
+    displayMessage(message);
+})
+
+// "custom-event" is an arbitrary name, anything could go here, same with the parameters. General shape of an emit.
+// socket.emit("custom-event", 25, "Socketlearn", {a:"a"})
 
 // When the form is submitted (send button is clicked)
 form.addEventListener("submit", event => {
@@ -25,6 +41,12 @@ form.addEventListener("submit", event => {
     // Display the message to the chatspace
     displayMessage(message)
 
+    // After we display the message, send the message to the backend. Add a room if we want the message to only go to a specific room.
+    socket.emit("send-message", message, room)
+
+    // Show the message on the browser console.
+    console.log(message);
+
     // Reset the input field
     messageInput.value = ""
 })
@@ -33,10 +55,17 @@ form.addEventListener("submit", event => {
 joinRoomButton.addEventListener("click", () => {
     // Store the value
     const room = roomInput.value;
+    // Emit a join-room request with the room value attached and a callback function.
+    socket.emit("join-room", room, message => {
+        displayMessage(message);
+    });
 })
 
 function displayMessage(message) {
+    // Create new div
     const div = document.createElement("div");
+    // Fill it with the message from the input field
     div.textContent = message;
+    // Append it to the chatspace.
     document.getElementById("chatspace").append(div)
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,38 @@
-const io = require("socket.io")(3000);
+// Create our Socket.io instance serverside with port and options object
+const io = require("socket.io")(3000, {
+    // Options object telling cors that its ok if people connect via 8080 even though we are hosted on 3000
+    cors: {
+        origin: ["http://localhost:8080"],
+    },
+});
 
+// On a succesfull connection to our declared port (8080)
 io.on("connection", socket => {
+    // Console log the id was connected to
     console.log(socket.id)
+
+    // .on referencing the custom event we declared on the client side and any parameters we included.
+    // In this case we are checking for message content and the room they intend the message for.
+    socket.on("send-message", (message, room) => {
+        // If no room is specified.
+        if (room === "") {
+            // BROADCAST (keyword and difference) the recieved message to every OTHER connected socket.
+            socket.broadcast.emit("recieve-message", message);
+        } else {
+            // Send the message to a specific room if one is given.
+            // .to functions the same as .broadcast in that it doesnt send the message to the requesting client.
+            socket.to(room).emit("recieve-message", message);
+        }
+        // Console log the message serverside.
+        console.log(message)
+    })
+
+    // When we get a join-room emit from the client side.
+    // The callback function must always be the last parameter passed in.
+    socket.on("join-room", (room, cb) => {
+        // Join them to that room
+        socket.join(room)
+        // Use our callback function (which displays a message on the client side.) To notify the user of what room they joined
+        cb(`Joined ${room}`)
+    })
 })
